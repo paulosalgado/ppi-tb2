404c296ba67dc1d6441cd12d1773a09a853bcf0c
paulosalgado93@hotmail.com <paulosalgado93@hotmail.com@55f2436e-ad5c-526d-8a9e-58ec519a8f8c>
2013-05-29 19:33:45 +0000

    Trabalho atual.

--- trunk/Trabalho_Bimestre2/src/Cliente.java
+++ trunk/Trabalho_Bimestre2/src/Cliente.java
@@ -5,24 +5,31 @@ public class Cliente {
 	private String nome;
 	private String endereco;
 	private String contato;
+	
 	public int getCodigo() {
 		return codigo;
 	}
+	
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getEndereco() {
 		return endereco;
 	}
+	
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
+	
 	public String getContato() {
 		return contato;
 	}
@@ -30,6 +37,14 @@ public class Cliente {
 		this.contato = contato;
 	}
 	
+	/**
+	 * Construtor com parâmetros.
+	 * 
+	 * @param codigo - o código do cliente.
+	 * @param nome - o nome do cliente.
+	 * @param endereco - o endereço do cliente.
+	 * @param contato - o contato do cliente.
+	 */
 	public Cliente(int codigo, String nome, String endereco, String contato) {
 		this.codigo = codigo;
 		this.nome = nome;
--- trunk/Trabalho_Bimestre2/src/OrdemDeServico.java
+++ trunk/Trabalho_Bimestre2/src/OrdemDeServico.java
@@ -1,6 +1,5 @@
 import java.util.Date;
 
-
 public class OrdemDeServico {
 
 	private int codigo;
@@ -10,50 +9,64 @@ public class OrdemDeServico {
 	private Date dataEnceramento;
 	private ListaEncadeadaDinamica<Cliente> listaClientesEnvolvidos;
 	private String prioridade;
+	
 	public int getCodigo() {
 		return codigo;
 	}
+	
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+	
 	public String getDescricao() {
 		return descricao;
 	}
+	
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 	public String getDescricaoAtendimento() {
 		return descricaoAtendimento;
 	}
+	
 	public void setDescricaoAtendimento(String descricaoAtendimento) {
 		this.descricaoAtendimento = descricaoAtendimento;
 	}
+	
 	public Date getDataAbertura() {
 		return dataAbertura;
 	}
+	
 	public void setDataAbertura(Date dataAbertura) {
 		this.dataAbertura = dataAbertura;
 	}
+	
 	public Date getDataEnceramento() {
 		return dataEnceramento;
 	}
+	
 	public void setDataEnceramento(Date dataEnceramento) {
 		this.dataEnceramento = dataEnceramento;
 	}
+	
 	public ListaEncadeadaDinamica<Cliente> getListaClientesEnvolvidos() {
 		return listaClientesEnvolvidos;
 	}
-	public void setListaClientesEnvolvidos(
-			ListaEncadeadaDinamica<Cliente> listaClientesEnvolvidos) {
+	public void setListaClientesEnvolvidos(ListaEncadeadaDinamica<Cliente> listaClientesEnvolvidos) {
 		this.listaClientesEnvolvidos = listaClientesEnvolvidos;
 	}
+	
 	public String getPrioridade() {
 		return prioridade;
 	}
+	
 	public void setPrioridade(String prioridade) {
 		this.prioridade = prioridade;
 	}
 	
+	/**
+	 * Construtor padrão.
+	 */
 	public OrdemDeServico() {
 		this.listaClientesEnvolvidos = new ListaEncadeadaDinamica<Cliente>();
 	}
